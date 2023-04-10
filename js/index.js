c6725c4788557defa3b2a2abca502f9248bfdedf
Mirza Rashed Baig <105800238+mirzarashidbaig@users.noreply.github.com>
2023-04-10 22:25:07 +0530

    js update

--- js/index.js
+++ js/index.js
@@ -1,27 +1,39 @@
 
 
+let inpd = document.querySelector('#date');
+let endDate = inpd;
+let input = document.getElementsByClassName("inputs");
+
 function countdown(){
-    const inpd = document.querySelector('#date').value;
-    const endDate = inpd;
-    
-    const input = document.getElementsByClassName("inputs");
-    const end = new Date(endDate);
+    const end = new Date(endDate.value);
+    save();
     const now = new Date();
     const diff = (end-now)/1000;
-    
     const days = Math.floor((diff/3600)/24);
-    
     const hours = Math.floor(diff/60/60)%24;
     const minutes = Math.floor(diff/60)%60;
     const second = Math.floor(diff%60);
-    
     input[0].value = days
     input[1].value = hours
     input[2].value = minutes
     input[3].value = second
-
+    
     setInterval(() => {
         countdown();
     }, 1000);
+    
+    
 }
+
+
+function save(){
+    localStorage.setItem("data", endDate.value);
+    
+}
+
+function show(){
+    endDate.value = localStorage.getItem("data")
+}
+
+show();
 
